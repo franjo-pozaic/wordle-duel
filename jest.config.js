94242242a39234b78b84cd27c9c7cb930a94d0fd
Franjo Pozaic <franjo.pozaic@gmail.com>
2024-10-29 14:01:50 +0100

    delete jest config, doesnt make a difference for running the tests

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,10 @@ export default {
     transformIgnorePatterns: [
         'node_modules/(?!(some-esm-module|another-esm-module)/)' // If needed to transpile ESM modules
     ],
+
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.js',
+    },
 };
