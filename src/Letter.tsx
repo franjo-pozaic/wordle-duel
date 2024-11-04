626b9f5b7c3e07994d7198bec1f2614556cf43d8
Franjo Pozaic <franjo.pozaic@gmail.com>
2024-11-04 11:51:11 +0100

    refactor styles

--- src/Letter.tsx
+++ src/Letter.tsx
@@ -10,7 +10,7 @@ export const Letter: React.FC<LetterProps> = ({ char, status }) => {
     const classByStatus: Record<Status, string> = {
         CORRECT: 'correct',
         PARTIAL: 'partial',
-        INITIAL: '',
+        INITIAL: 'initial',
         WRONG: 'wrong'
     }
     return (
