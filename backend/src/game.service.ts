17e28918d87c64aecba24b4e2e8d05fa19e4c4f5
Franjo Pozaic <franjo.pozaic@gmail.com>
2025-01-17 17:35:02 +0100

    controller chooses random word

--- backend/src/game.service.ts
+++ backend/src/game.service.ts
@@ -3,14 +3,49 @@ import { randomUUID } from "crypto";
 import { GameRepository } from "./game.repository";
 import { Game } from "./game.interface";
 
+const wordleWords = [
+    "apple",
+    "grape",
+    "house",
+    "zebra",
+    "piano",
+    "jumps",
+    "truck",
+    "flute",
+    "brick",
+    "spice",
+    "crane",
+    "trace",
+    "sheep",
+    "vivid",
+    "plane",
+    "table",
+    "cloud",
+    "beach",
+    "shiny",
+    "brush",
+    "tiger",
+    "chair",
+    "green",
+    "spark",
+    "robot",
+    "frame",
+    "music",
+    "queen",
+    "quick",
+    "glass"
+  ];
+  
 
 @Injectable()
 export class GameService {
     constructor(private gameRepository: GameRepository) {}
 
     createGame() {
+        const randomWord = wordleWords[Math.floor(Math.random() * wordleWords.length)];
+
         const game: Game = {
-            word: 'BEAST',
+            word: randomWord.toUpperCase(),
             created: new Date(),
             id: randomUUID(),
             players: []
